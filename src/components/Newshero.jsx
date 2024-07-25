c387e37849f3ec07dad06687734c95914bece814
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-07-25 14:26:54 +0000

    feat: Add a news card to display  trending news or news header.

---
+++ src/components/Newshero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import event from "../assets/events.jpeg";
+
+const Newshero = () => {
+  return (
+    <div>
+      <div className="rounded-lg bg-slate-600 m-6 bg-cover"
+      style={{ backgroundImage: `url(${event})` }}>
+        <div className="object-fill w-[310px] h-[280px] m-6 "
+      >
+    
+        </div>
+        <div className="">
+          <p className="text-xl tracking-wider text-white text-center">
+            Behind the Covers: Inspired By True Stories
+          </p>
+          <p className="text-[11px] tracking-wider text-white text-center">
+            35minutes ago by <b className="text-orange-500">Kareem Rashina</b>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Newshero;
