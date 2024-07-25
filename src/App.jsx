549c872479dba7aa3fb4f24ddb7d6447e96b23c4
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-07-25 14:30:51 +0000

    feat: Include footer in App.js routing.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import TeamsPage from './pages/TeamsPage';
 import ContactPage from './pages/ContactPage';
 import AwstorePage from './pages/AwstorePage';
 import NewsPage from './pages/NewsPage';
+import Footer from './common/Footer';
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
       <Route path = "awstore" element = {<AwstorePage/>}/>
       <Route path = "NewsPage" element = {<NewsPage/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
