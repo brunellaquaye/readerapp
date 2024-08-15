a8905f4794d33d6ba7b7ce601dc9e81dbf88b8dd
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-08-15 13:14:19 +0000

    remove background color from navbar.

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-red-400">
+    <nav className="">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-cent justify-between h-16">
           <div className="flex items-center">
