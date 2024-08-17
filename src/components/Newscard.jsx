6a83a92f4a0795a25a3d76d99a16c0d96db824f0
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-08-17 09:31:14 +0000

    Update news feed page.

--- src/components/Newscard.jsx
+++ src/components/Newscard.jsx
@@ -4,7 +4,7 @@ import news from "../assets/news.jpeg";
 const Newscard = ({ image, title, description }) => {
   return (
     <div
-      className="p-6 shadow-md rounded-xl"
+      className="p-6 shadow-md rounded-xl hover:shadow-2xl transition duration-500 ease-in-out"
     >
       {/* <div
     className=''
@@ -12,21 +12,21 @@ const Newscard = ({ image, title, description }) => {
     ></div> */}
       <img src={image} className="rounded-xl" />
       <div className="flex text-[11px] mt-2 ">
-        <img src={news} className="rounded-full w-5 h-5 mr-2" />
-        <p className="mr-2 md:text-lg">Netflix</p>
+        {/* <img src={news} className="rounded-full w-5 h-5 mr-2" /> */}
+        <p className="mr-2 md:text-lg">Info</p>
         <div className="w-1 h-1 rounded-full bg-black mt-2 mr-12"></div>
-        <p className="md:text-lg">12 minutes ago</p>
+        <p className="md:text-lg">2 minutes ago</p>
       </div>
-
+        <div className="mb-4">
       <a className="text-lg tracking-wide hover:underline md:text-4xl ">
         {title}
       </a>
+      </div>
       <p className="text-sm md:text-2xl ">{description}</p>
 
       <div className="flex text-[11px]">
         <p className="mr-2 text-orange-600 md:text-lg">Books</p>
-        <div className="w-1 h-1 rounded-full bg-black mt-2 mr-12 "></div>
-        <p className="md:text-lg">12 min read</p>
+        {/* <div className="w-1 h-1 rounded-full bg-black mt-2 mr-12 "></div> */}
       </div>
     </div>
   );
