0e937b49d3f06ea3a2f407a317c186d27416e224
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-08-19 01:22:58 +0000

    fix: Improve layout of webpage.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,13 @@
-import React from "react";
 // import Header from "../components/Header";
 import "../styleSheet/LandingPage.css";
 import firstImage from "../assets/Rectangle 1034.png";
 import secondImage from "../assets/Rectangle 1035.png";
 import thirdImage from "../assets/Rectangle 1036.png";
 import arrow1 from "../assets/Arrow 1.png";
-import arrow2 from "../assets/Arrow 2.png";
-import polygon from "../assets/Polygon 2.png";
+
 const HomePage = () => {
   return (
-    <div className="LandingPageWrapper">
+    <div className="">
       <section className="overlayContainer">
         {/* <Header pageName="Contact Us" /> */}
         <div className="overlayText">
@@ -22,127 +20,43 @@ const HomePage = () => {
       <section className="displayOfArtifactsSection">
         <p>Explore tons of digital</p>
         <p>art,books and paintings.</p>
-        <div className="sectionImagesContainer">
-          <img src={firstImage} alt="artifacts" />
-          <img src={secondImage} style={{ marginTop: "5%" }} alt="artifacts" />
-          <img src={thirdImage} style={{ marginTop: "10%" }} alt="artifacts" />
+        <div className="flex flex-col lg:flex-row justify-between items-center w-[90%] mx-auto mt-2 pl-6 ">
+          <img src={firstImage} alt="artifacts" className="aspect-square h-70 w-70 md:h-80 md:w-80" />
+          <img src={secondImage} alt="artifacts" className="aspect-square h-70 w-70 md:h-80 md:w-80" />
+          <img src={thirdImage} alt="artifacts" className="aspect-square h-70 w-70 md:h-80 md:w-80" />
         </div>
       </section>
-      <section className="aboutSection">
-        <div
-          className="polygons"
-          style={{ position: "absolute", top: "38%", left: "-10%" }}
-        >
-          <img
-            src={polygon}
-            style={{
-              width: "21.6px",
-              height: "31.86px",
-            }}
-            alt="arrow"
-          />
+      <section className="w-[90%] mx-auto">
+        <div className="font-extrabold text-5xl">
+          <p style={{ textAlign: "center" }}>About Us</p>
         </div>
-        <div
-          className="polygons"
-          style={{
-            position: "absolute",
-            left: "70%",
-            top: "-20%",
-            transform: "rotate(220deg)",
-          }}
-        >
-          <img
-            src={polygon}
-            style={{ width: "21.6px", height: "31.86px" }}
-            alt="arrow"
-          />
-        </div>
-        <div
-          className="polygons"
-          style={{
-            position: "absolute",
-            left: "50%",
-            top: "100%",
-            transform: "rotate(250deg)",
-          }}
-        >
-          <img
-            src={polygon}
-            style={{ width: "21.6px", height: "31.86px" }}
-            alt="arrow"
-          />
-        </div>
-        <div className="aboutHeading">
-          <p style={{ textAlign: "left" }}>About</p>
-          <p style={{ textAlign: "right" }}>us</p>
-        </div>
-        <div className="aboutContent">
-          <p>
-          At ReaderApp, we believe in the power of stories to inspire, educate, and connect people.
-          Founded in 2018, our platform offers a vibrant marketplace where authors can showcase their work and readers can discover new voices.
-          With AwStore, our mobile app, we provide a unique space for book lovers to purchase, review, and engage directly with their favorite authors.
-          Whether you're an avid reader or an aspiring writer, ReaderApp is your gateway to a world of endless literary possibilities.
-          Join us on our mission to celebrate and share the written word.
+        <div className="text-center text-2xl my-3">
+          <p className="my-5">
+            At ReaderApp, we believe in the power of stories to inspire,
+            educate, and connect people. Founded in 2018, our platform offers a
+            vibrant marketplace where authors can showcase their work and
+            readers can discover new voices. With AwStore, our mobile app, we
+            provide a unique space for book lovers to purchase, review, and
+            engage directly with their favorite authors. Whether you're an avid
+            reader or an aspiring writer, ReaderApp is your gateway to a world
+            of endless literary possibilities. Join us on our mission to
+            celebrate and share the written word.
           </p>
-          <div className="learnMoreContainer">
-            <div
-              className="polygons"
-              style={{ position: "absolute", left: "-100%", top: "450%" }}
-            >
-              <img
-                src={polygon}
-                style={{ width: "21.6px", height: "31.86px" }}
-                alt="arrow"
-              />
-            </div>
-            <div
-              className="polygons"
-              style={{
-                position: "absolute",
-                right: "-90%",
-                top: "330%",
-                transform: "rotate(220deg)",
-              }}
-            >
-              <img
-                src={polygon}
-                style={{ width: "21.6px", height: "31.86px" }}
-                alt="arrow"
-              />
-            </div>
-            <div
-              className="polygons"
-              style={{
-                position: "absolute",
-                right: "-60%",
-                top: "470%",
-                transform: "rotate(250deg)",
-              }}
-            >
-              <img
-                src={polygon}
-                style={{ width: "21.6px", height: "31.86px" }}
-                alt="arrow"
-              />
-            </div>
+          <div className="mt-10 border rounded-md bg-[#1e1e1e] text-white flex items-center gap-2 w-fit p-2 px-7 mx-auto">
             <p>Learn More</p>
-            <img src={arrow2} alt="arrow to about us page" />
+            <span className="text-5xl h-14">&#8594;</span>{" "}
           </div>
         </div>
       </section>
-      <section className="discoverySection">
-        <p>Let's discover something </p>
-        <p>
-          <em>cool</em> together
-        </p>
-        <div className="mobileAppDownloadContainer">
+      <section className="mb-10 border rounded-md bg-[#1e1e1e] text-3xl text-white flex flex-col items-center gap-2 w-fit p-4 px-7 mx-auto">
+        <p className="text-center">Let's discover something cool together</p>
+        <div className="flex items-center gap-3">
           <p>Download Mobile app</p>
           <img src={arrow1} alt="arrow to mobile app" />
         </div>
       </section>
     </div>
   );
+};
 
-}
-
-export default HomePage
+export default HomePage;
